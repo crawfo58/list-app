9874f8f2f06b52d0bcb268a6ce36268a1cffdabe
John <crawfo58@purdue.edu>
2018-05-17 10:02:29 -0400

    Change html with boilerplate and Foundation

--- list.js
+++ list.js
@@ -1,22 +1,33 @@
 const changeText = function(ev) {
     ev.preventDefault()
-    const users = document.querySelector('#items')
+    const items = document.querySelector('#items')
     const name = form.name.value
-
-    const list = document.createElement('ul')
 
     const nameItem = document.createElement('li')
     nameItem.textContent = `Name: ${name}`
 
     list.appendChild(nameItem)
     array.push(name)
-    console.log(array)
 
-    users.appendChild(list)
+    items.appendChild(list)
 
     form.reset()
     form.name.focus()
 }
+
+const deleteItem = function() {
+    const items = document.querySelector('#items')
+    
+    const nameItem = document.createElement('li')
+    nameItem.textContent = `Name: ${array[array.length-1]}`
+    list.removeChild(nameItem)
+    array.pop()
+    command.log(array)
+}
+
+const list = document.createElement('ul')
 const array = []
 const form = document.querySelector('form#listForm')
 form.addEventListener('submit', changeText)
+form.addEventListener('click', deleteItem)
+
