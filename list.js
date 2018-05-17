90b4f28de899b3489c1c2e41408acb94bb6534f1
John <crawfo58@purdue.edu>
2018-05-17 10:20:24 -0400

    Change javascript to match new html

--- list.js
+++ list.js
@@ -1,33 +1,16 @@
-const changeText = function(ev) {
+const app = {
+  init:function(formSelector) {
+    document
+        .querySelector(formSelector)
+        .addEventListener('submit', this.handleSubmit)
+  },
+
+  handleSubmit: function(ev) {
     ev.preventDefault()
-    const items = document.querySelector('#items')
-    const name = form.name.value
-
-    const nameItem = document.createElement('li')
-    nameItem.textContent = `Name: ${name}`
-
-    list.appendChild(nameItem)
-    array.push(name)
-
-    items.appendChild(list)
-
-    form.reset()
-    form.name.focus()
+    const f = ev.target
+    console.log(f.itemName.value)
+  },
 }
 
-const deleteItem = function() {
-    const items = document.querySelector('#items')
-    
-    const nameItem = document.createElement('li')
-    nameItem.textContent = `Name: ${array[array.length-1]}`
-    list.removeChild(nameItem)
-    array.pop()
-    command.log(array)
-}
-
-const list = document.createElement('ul')
-const array = []
-const form = document.querySelector('form#listForm')
-form.addEventListener('submit', changeText)
-form.addEventListener('click', deleteItem)
+app.init('#listForm')
 
