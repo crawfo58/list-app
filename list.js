e37f31c8eafc2d6967d8cfc4fc426ba85de8e393
John <crawfo58@purdue.edu>
2018-05-17 11:24:58 -0400

    Add items to an array

--- list.js
+++ list.js
@@ -1,5 +1,6 @@
 const app = {
   init(selectors) {
+    this.items = []
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
 
@@ -23,6 +24,8 @@ const app = {
         id: ++this.max, 
         name: f.itemName.value,
     }
+
+    this.items.push(item)
     
     const item2 = this.renderListItem(item)
     this.list.appendChild(item2)
