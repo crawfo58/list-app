b101521f76b2911b35cab149b8d8f3fd170ff16a
John <crawfo58@purdue.edu>
2018-05-21 02:18:23 -0400

    Make delete button work

--- list.js
+++ list.js
@@ -1,46 +1,66 @@
 const app = {
-  init(selectors) {
-    this.items = []
-    this.max = 0
-    this.list = document.querySelector(selectors.listSelector)
-    this.template = document.querySelector(selectors.templateSelector)
+    init(selectors) {
+      this.items = []
+      this.max = 0
+      this.list = document.querySelector(selectors.listSelector)
+      this.template = document.querySelector(selectors.templateSelector)
+  
+      document
+          .querySelector(selectors.formSelector)
+          .addEventListener('submit', ev => {
+              ev.preventDefault()
+              this.handleSubmit(ev)
+          })
+    },
+  
+    renderListItem(item) {
+      const item2 = this.template.cloneNode(true)
+      item2.classList.remove('template')
+      item2.dataset.id = item.id
+      item2
+          .querySelector('.itemName')
+          .textContent = item.name
+      item2
+          .querySelector('button.alert.button')
+          .addEventListener('click', (ev)=> {
+              ev.preventDefault()
+              this.removeListItem(ev)
+          })
+      return item2
+    }, 
 
-    document
-        .querySelector(selectors.formSelector)
-        .addEventListener('submit', ev => {
-            ev.preventDefault()
-            this.handleSubmit(ev)
-        })
-  },
-
-  renderListItem(item) {
-    const item2 = this.template.cloneNode(true)
-    item2.classList.remove('template')
-    item2.dataset.id = item.id
-    item2
-        .querySelector('.itemName')
-        .textContent = item.name
-    return item2
-  }, 
-
-  handleSubmit(ev) {
-    const f = ev.target
-    const item = {
-        id: ++this.max, 
-        name: f.itemName.value,
-    }
-
-    this.items.unshift(item)
-    
-    const item2 = this.renderListItem(item)
-    this.list.insertBefore(item2, this.list.firstElementChild)
-    f.reset()
-  },
-}
-
-app.init({
-    formSelector: '#itemForm',
-    listSelector: '#itemList',
-    templateSelector: '.item.template', 
-})
-
+    removeListItem(ev) {
+        ev.preventDefault()
+        const button = ev.target
+        const item2 = button.parentNode.parentNode
+        item2.remove()
+        for(i = 0; i < this.items.length; i++) {
+            const index = this.items[i].id.toString()
+            if(item2.dataset.id === index) {
+                this.items.splice(i,1)
+                break
+            }
+        }
+    },
+  
+    handleSubmit(ev) {
+      const f = ev.target
+      const item = {
+          id: ++this.max, 
+          name: f.itemName.value,
+      }
+  
+      this.items.unshift(item)
+      
+      const item2 = this.renderListItem(item)
+      this.list.insertBefore(item2, this.list.firstElementChild)
+      f.reset()
+    },
+  }
+  
+  app.init({
+      formSelector: '#itemForm',
+      listSelector: '#itemList',
+      templateSelector: '.item.template', 
+  })
+  
